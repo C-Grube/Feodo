01699fce37bd065f916dcc3c081c7e5b794c7904
C-Grube <cgrube421@gmail.com>
2021-09-03 08:33:27 -0500

    Update ioc_parse.py

--- ioc_parse.py
+++ ioc_parse.py
@@ -14,14 +14,14 @@ def days_delta(day_one, day_two):
 # Returns a dictionary where the IP address is the key and the entire list (each line in the CSV from the site) is the value.
 def generate_ioc_dict():
     data = requests.get('https://feodotracker.abuse.ch/downloads/ipblocklist.csv').text.splitlines()
-    indicator_list = [line for line in data if "#" not in line]
+    indicator_list = [line.strip("\"") for line in data if "#" not in line if "first_seen_utc" not in line]
     ioc_dict = {}
     for ioc in indicator_list:
         ioc_list = ioc.replace(" ", ",").split(",")
         ioc_dict[ioc_list[2]] = ioc_list
     return(ioc_dict)
-
-
+    
+    
 def main():
     todays_date = datetime.now().strftime('%Y-%m-%d')
     ioc_dict = generate_ioc_dict()
@@ -32,7 +32,7 @@ def main():
         else:
             print("%s IP address %s was first seen %s day(s) ago." %
                   (v[-1], v[2], days_between))
-
-
+                  
+                  
 if __name__ == '__main__':
     main()
